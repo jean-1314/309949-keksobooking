4336c48febf0ac520ba208d35ca4edafebfee56b
jean-1314 <isfiremsk@gmail.com>
2017-09-04 23:11:47 +0300

    Transfer functions to data

--- js/util.js
+++ js/util.js
@@ -1,10 +1,5 @@
 'use strict';
 (function () {
-  var ADS_NUMBER = 8;
-  var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-  var TYPES = ['flat', 'house', 'bungalo'];
-  var CHECKINOUT_TIME = ['12:00', '13:00', '14:00'];
-  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
   var PIN_RANGE = [300, 900, 100, 500]; // [xMin, xMax, yMin, yMax]
   var PIN_SIZE = [40, 40]; // width = 40px; height = 40px;
@@ -30,7 +25,6 @@
   var dialog = document.getElementById('offer-dialog');
   var dialogOff = document.querySelector('.dialog__close');
 
-
   function generateRandomNumber(min, max) {
     var multiplier = max + 1 - min;
     return Math.floor(min + Math.random() * multiplier);
@@ -48,96 +42,11 @@
     }
   }
 
-  function getAvatar(nameSuffix) {
-    return 'img/avatars/user0' + nameSuffix + '.png';
-  }
-
-  function getTitle() {
-    return TITLES[Math.floor(Math.random() * TITLES.length)];
-  }
-
-  function getPrice() {
-    var minPrice = 1000;
-    var maxPrice = 1000000;
-    return generateRandomNumber(minPrice, maxPrice);
-  }
-
-  function getType() {
-    return TYPES[Math.floor(Math.random() * TYPES.length)];
-  }
-
-  function getRooms() {
-    var minRooms = 1;
-    var maxRooms = 5;
-    return generateRandomNumber(minRooms, maxRooms);
-  }
-
-  function getGuests() {
-    var minGuests = 1;
-    var maxGuests = 10;
-    return generateRandomNumber(minGuests, maxGuests);
-  }
-
-  function getCheckInOut() {
-    return CHECKINOUT_TIME[Math.floor(Math.random() * CHECKINOUT_TIME.length)];
-  }
-
-  function getFeatures() {
-    var randFeaturesNumber = generateRandomNumber(0, FEATURES.length - 1);
-    shuffle(FEATURES);
-    return FEATURES.slice(0, randFeaturesNumber);
-  }
-
-  function getLocationX() {
-    var pinX = PIN_SIZE[0] / 2;
-    return Math.floor(Math.random() * (PIN_RANGE[1] - PIN_RANGE[0] - pinX) + PIN_RANGE[0]) + pinX;
-  }
-
-  function getLocationY() {
-    var pinY = PIN_SIZE[1] / 2;
-    return Math.floor(Math.random() * (PIN_RANGE[3] - PIN_RANGE[2] - pinY) + PIN_RANGE[2]) + pinY;
-  }
-
-  function getRentAd(params) {
-    var locationX = getLocationX();
-    var locationY = getLocationY();
-
-    return {
-      'author': {
-        'avatar': getAvatar(params.number)
-      },
-
-      'offer': {
-        'title': getTitle(),
-        'address': '' + locationX + ', ' + locationY + '',
-        'price': getPrice(),
-        'type': getType(),
-        'rooms': getRooms(),
-        'guests': getGuests(),
-        'checkin': getCheckInOut(),
-        'checkout': getCheckInOut(),
-        'features': getFeatures(),
-        'description': '',
-        'photos': []
-      },
-
-      'location': {
-        'x': locationX,
-        'y': locationY
-      }
-    };
-  }
-
-  function getRentAds() {
-    var adItems = [];
-    for (var i = 0; i < ADS_NUMBER; i++) {
-      adItems.push(getRentAd({number: i + 1}));
-    }
-    return adItems;
-  }
-
   window.util = {
-    getAdsNumber: ADS_NUMBER,
+
+    pinRange: PIN_RANGE,
+
+    pinSize: PIN_SIZE,
 
     esc: ESC_KEYCODE,
 
@@ -151,6 +60,8 @@
 
     getDialogOff: dialogOff,
 
-    getRentAds: getRentAds()
+    getRandomNumber: generateRandomNumber,
+
+    shuffle: shuffle
   };
 })();
