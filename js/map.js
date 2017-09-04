888bc2f50b7c29c861a9762bb151a80be42fc2a4
jean-1314 <isfiremsk@gmail.com>
2017-09-05 00:06:32 +0300

    Add drag'n'drop function

--- js/map.js
+++ js/map.js
@@ -12,6 +12,53 @@
 
   init();
 
+  (function () {
+    var pinMain = document.querySelector('.pin__main');
+
+    pinMain.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
+
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+
+        pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+        pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
+
+    var map = window.pin.pinMap;
+
+    map.addEventListener('dragover', function (evt) {
+      evt.preventDefault();
+      return false;
+    });
+  })();
+
   window.map = {
     getOpenDialog: openDialog
   };
