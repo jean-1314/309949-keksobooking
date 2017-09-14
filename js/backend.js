e2bba468a43013228de05a654e2f75947ae98ecb
jean-1314 <isfiremsk@gmail.com>
2017-09-15 01:11:54 +0300

    Minor changes

--- js/backend.js
+++ js/backend.js
@@ -28,10 +28,12 @@
     return xhr;
   };
 
+
   window.backend = {
 
     load: function load(onLoad, onError) {
       var xhr = setup(onLoad, onError);
+
       xhr.open('GET', SERVER_URL + '/data');
       xhr.send();
     },
@@ -41,5 +43,6 @@
       xhr.open('POST', SERVER_URL);
       xhr.send(data);
     }
+
   };
 })();
