653927c8da21a11d705af5201c32b4c7f1101ccc
jean-1314 <isfiremsk@gmail.com>
2017-09-17 21:20:23 +0300

    get variable out of function

--- js/card.js
+++ js/card.js
@@ -1,10 +1,11 @@
 'use strict';
 (function () {
 
-  function createDialog(data) {
-    var template = document.getElementById('lodge-template').cloneNode(true);
+  var template = document.getElementById('lodge-template');
 
-    var type = template.content.querySelector('.lodge__type');
+  function createDialog(data) {
+    var templateClone = template.cloneNode(true);
+    var type = templateClone.content.querySelector('.lodge__type');
     switch (data.offer.type) {
       case 'bungalo':
         type.textContent = 'Лачуга';
@@ -22,22 +23,27 @@
         type.textContent = 'Неопознанный тип жилья';
     }
 
-    template.content.querySelector('.lodge__title').textContent = data.offer.title;
-    template.content.querySelector('.lodge__address').textContent = data.offer.address;
-    template.content.querySelector('.lodge__price').innerHTML = data.offer.price + ' &#x20bd;/ночь';
-    template.content.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + data.offer.guests + ' гостей в ' + data.offer.rooms + ' комнатах';
-    template.content.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + data.offer.checkin + ' выезд до ' + data.offer.checkout + '';
+    templateClone.content.querySelector('.lodge__title').textContent = data.offer.title;
+    templateClone.content.querySelector('.lodge__address').textContent = data.offer.address;
+    templateClone.content.querySelector('.lodge__price').textContent = data.offer.price + ' ₽/ночь';
+    templateClone.content.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + data.offer.guests + ' гостей в ' + data.offer.rooms + ' комнатах';
+    templateClone.content.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + data.offer.checkin + ' выезд до ' + data.offer.checkout + '';
 
-    var lodgeFeatures = template.content.querySelector('.lodge__features');
+    var lodgeFeatures = templateClone.content.querySelector('.lodge__features');
+    var fragment = document.createDocumentFragment();
     for (var i = 0; i < data.offer.features.length; i++) {
-      lodgeFeatures.innerHTML += '<span class="feature__image feature__image--' + data.offer.features[i] + '"></span>';
+      var span = document.createElement('span');
+      span.classList.add('feature__image');
+      span.classList.add('feature__image--' + data.offer.features[i] + '');
+      fragment.appendChild(span);
     }
+    lodgeFeatures.appendChild(fragment);
 
-    template.content.querySelector('.lodge__description').textContent = data.offer.description;
+    templateClone.content.querySelector('.lodge__description').textContent = data.offer.description;
 
     window.util.getDialog.querySelector('.dialog__title img').src = data.author.avatar;
 
-    window.util.getDialog.replaceChild(template.content, window.util.getDialog.children[1]);
+    window.util.getDialog.replaceChild(templateClone.content, window.util.getDialog.children[1]);
   }
 
   window.card = {
